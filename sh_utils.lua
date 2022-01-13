6baa6f1e1d5245622fd03c5b7addaeee99651365
webnoise <wwqwind@gmail.com>
2022-01-13 14:40:49 +0300

    Update sh_utils.lua

--- sh_utils.lua
+++ sh_utils.lua
@@ -4,7 +4,7 @@ function util.GetEntitySides(entity)
 	local pos, ang = entity:GetPos(), entity:GetAngles() 
 	local sides = {
 		top = pos + (ang:Forward() * min.x),
-		bottom = pos + (ang:Forward() * -min.x,
+		bottom = pos + (ang:Forward() * -min.x),
 		left = pos + (ang:Right() * -min.y),
 		right = pos + (ang:Right() * min.y)
 	}
@@ -15,9 +15,9 @@ end
 function util.RandomPosOnEntity(entity, spacing)
 	local pos = entity:GetPos()
 	
-	local parts = util.GetEntitySides(entity)
-	local top, bottom = parts.top, parts.bottom
-	local left, right = parts.left, parts.right
+	local sidess = util.GetEntitySides(entity)
+	local top, bottom = sides.top, sides.bottom
+	local left, right = sides.left, sides.right
 	
 	local newX = math.random(math.min(top.x + spacing, bottom.x - spacing), math.max(top.x + spacing, bottom.x - spacing))
 	local newY = math.random(math.min(left.y + spacing, right.y - spacing), math.max(left.y + spacing, right.y - spacing))
